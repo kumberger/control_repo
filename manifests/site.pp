cb232e392ab578f327ba945f641cbdebf14da3d4
kumberger <36010766+kumberger@users.noreply.github.com>
2019-03-15 14:25:43 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,7 @@ node default {
   }
 node 'master.puppet.vm' {
   include role::master_server
-  file { '/root/READMS';
+  file { '/root/README':
     ensure  => file,
     content => "Welcome to ${fqdn}"\n,
  }
